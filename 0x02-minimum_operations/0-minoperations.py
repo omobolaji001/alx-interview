66d31f322d7779f764f461eded98a02a1915aa4e
omobolaji001 <omobolajiolayiwola11@gmail.com>
2024-04-24 00:03:09 +0100

    Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -10,11 +10,11 @@ def minOperations(n):
     Returns the fewest number of operations needed to result in exactly
     n H characters in the file.
     """
-    total_operations = 0
+    min_operations = 0
     operations = 2
     while n > 1:
         while n % operations == 0:
-            total_operations += operations
+            min_operations += operations
             n /= operations
         operations += 1
-    return total_operations
+    return min_operations
